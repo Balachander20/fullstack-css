87e64b582e55569cc7b45d2c4fc3f79a3387119c
balachander20 <balachander7315@gmailcom>
2021-12-07 18:17:32 +0530

    routing

---
+++ database/schema/schema..js
@@ -0,0 +1,20 @@
+
+// products
+/*
+id:id
+name:string
+price:number
+category:category_id
+*/
+// category
+/*
+id:id
+name:string
+*/
+
+// order
+/*
+price:number
+product :array of products[product_id]
+
+*/
