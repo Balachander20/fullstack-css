955c69962113b8034b7283434e09497fefb409e7
balachander20 <balachander7315@gmailcom>
2021-12-08 18:13:11 +0530

    updated server

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 const express=require("express")
 // const database =require("./database/db")
 const categoryroutes =require("./routes/categoryroutes")
+const productroutes =require("./routes/productRoutes")
 const app=express()
 app.use(express.json())
 // console.log(categoryroutes.stack)
 app.use("/category",categoryroutes)
+app.use("/products",productroutes)
 // app.get("/",(req,res)=>{
   
 //   try{
